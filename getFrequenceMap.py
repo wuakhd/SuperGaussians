c285f979294100fb7018d6f548a2a33b124b86ec
wuakhd <wuhe1209331352@gmail.com>
2024-12-30 16:56:35 +0800

    Add files via upload

---
+++ getFrequenceMap.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import torch
+from torchvision import transforms
+
+
+def gaussian_filter_high_f(fshift, D):
+    # 获取索引矩阵及中心点坐标
+    h, w = fshift.shape
+    # 创建 x 和 y 的网格坐标
+    x, y = np.mgrid[0:h, 0:w]
+    center = (int((h - 1) / 2), int((w - 1) / 2))
+    # 计算中心距离矩阵
+    dis_square = (x - center[0]) ** 2 + (y - center[1]) ** 2
+    # 计算变换矩阵
+    template = np.exp(- dis_square / (2 * D ** 2))
+
+    return template * fshift
+
+
+def gaussian_filter_low_f(fshift, D):
+    # 获取索引矩阵及中心点坐标
+    h, w = fshift.shape
+    x, y = np.mgrid[0:h, 0:w]
+    center = (int((h - 1) / 2), int((w - 1) / 2))
+
+    # 计算中心距离矩阵
+    dis_square = (x - center[0]) ** 2 + (y - center[1]) ** 2
+
+    # 计算变换矩阵
+    template = 1 - np.exp(- dis_square / (2 * D ** 2))  # 高斯过滤器
+
+    return template * fshift
+
+
+def circle_filter_high_f(fshift, radius_ratio):
+    """
+    过滤掉除了中心区域外的高频信息
+    """
+    # 1, 生成圆形过滤器, 圆内值1, 其他部分为0的过滤器, 过滤
+    template = np.zeros(fshift.shape, np.uint8)
+    crow, ccol = int(fshift.shape[0] / 2), int(fshift.shape[1] / 2)  # 圆心
+    radius = int(radius_ratio * img.shape[0] / 2)
+    if len(img.shape) == 3:
+        cv2.circle(template, (crow, ccol), radius, (1, 1, 1), -1)
+    else:
+        cv2.circle(template, (crow, ccol), radius, 1, -1)
+    # 2, 过滤掉除了中心区域外的高频信息
+    return template * fshift
+
+
+def circle_filter_low_f(fshift, radius_ratio):
+    """
+    去除中心区域低频信息
+    """
+    # 1 生成圆形过滤器, 圆内值0, 其他部分为1的过滤器, 过滤
+    filter_img = np.ones(fshift.shape, np.uint8)
+    crow, col = int(fshift.shape[0] / 2), int(fshift.shape[1] / 2)
+    radius = int(radius_ratio * img.shape[0] / 2)
+    if len(img.shape) == 3:
+        cv2.circle(filter_img, (crow, col), radius, (0, 0, 0), -1)
+    else:
+        cv2.circle(filter_img, (crow, col), radius, 0, -1)
+    # 2 过滤中心低频部分的信息
+    return filter_img * fshift
+
+
+def ifft(fshift):
+    """
+    傅里叶逆变换
+    """
+    ishift = np.fft.ifftshift(fshift)  # 把低频部分sift回左上角
+    iimg = np.fft.ifftn(ishift)  # 出来的是复数，无法显示
+    iimg = np.abs(iimg)  # 返回复数的模
+    return iimg
+
+
+def get_low_high_f(img, radius_ratio, D):
+    """
+    获取低频和高频部分图像
+    """
+    # 傅里叶变换
+    # np.fft.fftn
+    f = np.fft.fftn(img)  # Compute the N-dimensional discrete Fourier Transform. 零频率分量位于频谱图像的左上角
+    fshift = np.fft.fftshift(f)  # 零频率分量会被移到频域图像的中心位置，即低频
+
+    # 获取低频和高频部分
+    hight_parts_fshift = circle_filter_low_f(fshift.copy(), radius_ratio=radius_ratio)  # 过滤掉中心低频
+    low_parts_fshift = circle_filter_high_f(fshift.copy(), radius_ratio=radius_ratio)
+    hight_parts_fshift = gaussian_filter_low_f(fshift.copy(), D=D)
+    low_parts_fshift = gaussian_filter_high_f(fshift.copy(), D=D)
+
+    low_parts_img = ifft(low_parts_fshift)  # 先sift回来，再反傅里叶变换
+    high_parts_img = ifft(hight_parts_fshift)
+
+    # 显示原始图像和高通滤波处理图像
+    img_new_low = (low_parts_img - np.amin(low_parts_img)) / (np.amax(low_parts_img) - np.amin(low_parts_img) + 0.00001)
+    img_new_high = (high_parts_img - np.amin(high_parts_img) + 0.00001) / (
+            np.amax(high_parts_img) - np.amin(high_parts_img) + 0.00001)
+
+    # uint8
+    img_new_low = np.array(img_new_low * 255, np.uint8)
+    img_new_high = np.array(img_new_high * 255, np.uint8)
+    return img_new_low, img_new_high
+
+
+def getFreTensor(img):
+    # 假设输入的是tensor类型表示的RGB的图片
+    r, g, b = img[0], img[1], img[2]
+    gray_tensor = 0.299 * r + 0.587 * g + 0.114 * b
+    # 将结果转换为 NumPy ndarray
+    gray_image = gray_tensor.numpy()
+    low_freq_part_img, high_freq_part_img = get_low_high_f(gray_image, radius_ratio=radius_ratio,
+                                                           D=D)  # multi channel or single
+    transform_to = transforms.ToTensor()
+    return transform_to(high_freq_part_img)
+
+def grayToWeight(gray, scale = 2.0):
+    # 计算灰度图的权重，并将其映射到 [0, 2] 范围内
+    normalized_weight = (gray - gray.min()) / (gray.max() - gray.min())  # 归一化到 [0, 1]
+    weight = normalized_weight * scale  # 缩放到 [0, 2] 范围
+
+    # 扩展权重，使其与 RGB 图像的大小相匹配
+    # 这里假设灰度图像是单通道的，我们需要将权重扩展为 (C, H, W) 形状
+    weight_expanded = weight.unsqueeze(0).expand_as(rgb_target)  # 将权重扩展到 (3, H, W)
+    return weight_expanded
---
+++ trainWithFrequence.py
@@ -0,0 +1,323 @@
+import numpy as np
+import os
+import torch
+from random import randint
+import utils.utils_image as ImageUtils
+from einops import rearrange
+from utils.loss_utils import l1_loss, ssim
+from gaussian_renderer import render, network_gui
+import sys
+from scene import Scene, GaussianModel
+from utils.general_utils import safe_state
+import uuid
+from tqdm import tqdm
+from utils.image_utils import psnr, render_net_image
+from argparse import ArgumentParser, Namespace
+from arguments import ModelParams, PipelineParams, OptimizationParams
+
+try:
+    from torch.utils.tensorboard import SummaryWriter
+
+    TENSORBOARD_FOUND = True
+except ImportError:
+    TENSORBOARD_FOUND = False
+import matplotlib.pyplot as plt
+import copy
+from lpipsPyTorch import lpips
+import pynvml
+from PIL import Image
+#引入频率图计算
+import getFrequenceMap
+
+def convert_gray_to_cmap(img_gray, map_mode='jet', revert=False, vmax=None):
+    img_gray = copy.deepcopy(img_gray)
+    shape = img_gray.shape
+    cmap = plt.get_cmap(map_mode)
+    if vmax is not None:
+        img_gray = (img_gray / vmax).clip(0, 1)
+    else:
+        img_gray = img_gray / (np.max(img_gray) + 1e-6)
+    if revert:
+        img_gray = 1 - img_gray
+    colors = cmap(img_gray.reshape(-1))[:, :3]
+    # visualization
+    colors = colors.reshape(shape + tuple([3])) * 255
+    return colors
+
+
+def ensure_directory_exists(file_path):
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+
+def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint):
+    first_iter = 0
+    tb_writer = prepare_output_and_logger(dataset)
+    gaussians = GaussianModel(dataset.sh_degree)
+    scene = Scene(dataset, gaussians)
+    gaussians.training_setup(opt)
+    if checkpoint:
+        (model_params, first_iter) = torch.load(checkpoint)
+        gaussians.restore(model_params, opt)
+
+    bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
+    background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+
+    iter_start = torch.cuda.Event(enable_timing=True)
+    iter_end = torch.cuda.Event(enable_timing=True)
+
+    viewpoint_stack = None
+    ema_loss_for_log = 0.0
+    ema_dist_for_log = 0.0
+    ema_normal_for_log = 0.0
+
+    progress_bar = tqdm(range(first_iter, opt.iterations), desc="Training progress")
+    first_iter += 1
+    for iteration in range(first_iter, opt.iterations + 1):
+        # print(f"ITERATION {iteration}")
+
+        iter_start.record()
+
+        gaussians.update_learning_rate(iteration)
+
+        # Every 1000 its we increase the levels of SH up to a maximum degree
+        if iteration % 1000 == 0:
+            gaussians.oneupSHdegree()
+
+        # Pick a random Camera
+        if not viewpoint_stack:
+            viewpoint_stack = scene.getTrainCameras().copy()
+
+        viewpoint_cam = viewpoint_stack.pop(randint(0, len(viewpoint_stack) - 1))
+
+        render_pkg = render(viewpoint_cam, gaussians, pipe, background)
+        image, viewspace_point_tensor, visibility_filter, radii = render_pkg["render"], render_pkg["viewspace_points"], \
+        render_pkg["visibility_filter"], render_pkg["radii"]
+
+        gt_image = viewpoint_cam.original_image.cuda()
+        Ll1 = l1_loss(image, gt_image)
+        loss = (1.0 - opt.lambda_dssim) * Ll1 + opt.lambda_dssim * (1.0 - ssim(image, gt_image))
+
+        #修改了这里，添加了Loss权重
+        grayImage = getFreTensor(gt_image)
+        weightsFromGray = grayToWeight(grayImage)
+        loss = loss*weightsFromGray
+
+        # regularization
+        lambda_normal = opt.lambda_normal if iteration > 7000 else 0.0
+        lambda_dist = opt.lambda_dist if iteration > 3000 else 0.0
+
+        rend_dist = render_pkg["rend_dist"]
+        rend_normal = render_pkg['rend_normal']
+        surf_normal = render_pkg['surf_normal']
+        normal_error = (1 - (rend_normal * surf_normal).sum(dim=0))[None]
+        normal_loss = lambda_normal * (normal_error).mean()
+        dist_loss = lambda_dist * (rend_dist).mean()
+
+        # loss
+        total_loss = loss + dist_loss  # + normal_loss
+
+        total_loss.backward()
+
+        iter_end.record()
+
+        with torch.no_grad():
+            # Progress bar
+            ema_loss_for_log = 0.4 * loss.item() + 0.6 * ema_loss_for_log
+            ema_dist_for_log = 0.4 * dist_loss.item() + 0.6 * ema_dist_for_log
+            ema_normal_for_log = 0.0  # * normal_loss.item() + 0.6 * ema_normal_for_log
+
+            if iteration % 10 == 0:
+                loss_dict = {
+                    "Loss": f"{ema_loss_for_log:.{5}f}",
+                    "distort": f"{ema_dist_for_log:.{5}f}",
+                    "normal": f"{ema_normal_for_log:.{5}f}",
+                    "Points": f"{len(gaussians.get_xyz)}"
+                }
+                progress_bar.set_postfix(loss_dict)
+
+                progress_bar.update(10)
+            if iteration == opt.iterations:
+                progress_bar.close()
+
+            # Log and save
+            if tb_writer is not None:
+                tb_writer.add_scalar('train_loss_patches/dist_loss', ema_dist_for_log, iteration)
+                tb_writer.add_scalar('train_loss_patches/normal_loss', ema_normal_for_log, iteration)
+
+            training_report(tb_writer, iteration, Ll1, loss, l1_loss, iter_start.elapsed_time(iter_end),
+                            testing_iterations, scene, render, (pipe, background), gaussians)
+            if (iteration in saving_iterations):
+                print("\n[ITER {}] Saving Gaussians".format(iteration))
+                scene.save(iteration)
+
+            # Densification
+            if iteration < opt.densify_until_iter:
+                gaussians.max_radii2D[visibility_filter] = torch.max(gaussians.max_radii2D[visibility_filter],
+                                                                     radii[visibility_filter])
+                gaussians.add_densification_stats(viewspace_point_tensor, visibility_filter)
+
+                if iteration > opt.densify_from_iter and iteration % opt.densification_interval == 0:  # and args.max_gs> gaussians.get_xyz.shape[0]:
+                    size_threshold = 20 if iteration > opt.opacity_reset_interval else None
+                    gaussians.densify_and_prune(opt.densify_grad_threshold, opt.opacity_cull, scene.cameras_extent,
+                                                size_threshold, args.max_gs)
+
+                if iteration % opt.opacity_reset_interval == 0 or (
+                        dataset.white_background and iteration == opt.densify_from_iter):
+                    gaussians.reset_opacity()
+
+            freq_vis_img = 1000
+            if (iteration % freq_vis_img == 0):
+                # test views
+                usetestviews = False
+                if (usetestviews):
+                    viewpoint_stack_test = scene.getTestCameras().copy()
+
+                    viewpoint_cam_test = viewpoint_stack_test.pop(randint(0, len(viewpoint_stack_test) - 1))
+
+                    render_pkg_test = render(viewpoint_cam_test, gaussians, pipe, background)
+                    image_test, viewspace_point_tensor_test, visibility_filter_test, radii_test = render_pkg_test[
+                        "render"], render_pkg_test["viewspace_points"], render_pkg_test["visibility_filter"], \
+                    render_pkg_test["radii"]
+
+                    gt_image_test = viewpoint_cam_test.original_image.cuda()
+
+                    image = image_test
+                    gt_image = gt_image_test
+
+                # normal_wrt = normal2rgb(normal.detach(), mask_vis.detach())
+                # depth_wrt = depth2rgb(depth.detach())
+
+                # get error image between gt and render
+                error_img = torch.abs(gt_image - image)
+                error_img = error_img.mean(axis=0)
+                color_map = convert_gray_to_cmap(error_img.detach().cpu(), map_mode='jet', revert=False, vmax=1)
+                # error_img = error_img.unsqueeze(0)
+                # error_img = error_img.repeat(3,1,1)
+                # change error image to blue and red color map
+
+                lis_img = [gt_image, image]
+                lis_img_new = []
+                for img in lis_img:
+                    if img is None:
+                        continue
+                    img = rearrange(img, 'c h w -> h w c').detach().cpu().numpy().clip(0, 1) * 255
+                    lis_img_new.append(img)
+                # error_img = rearrange(error_img, 'c h w -> h w c').detach().cpu().numpy().clip(0,1)*255
+                # error_img = error_img / error_img.max() * 255
+                # re scale error image to white and black
+
+                lis_img_new.append(color_map)
+                dir_exp_img = scene.model_path
+                ImageUtils.write_image_lis(f'{dir_exp_img}/test/iter{iteration:06d}.png',
+                                           lis_imgs=lis_img_new,
+                                           color_space='RGB',
+                                           reso_downsample=-1,
+                                           add_black_border=True
+                                           )
+            # Optimizer step
+            if iteration < opt.iterations:
+                gaussians.optimizer.step()
+                gaussians.optimizer.zero_grad(set_to_none=True)
+
+            if (iteration in checkpoint_iterations):
+                print("\n[ITER {}] Saving Checkpoint".format(iteration))
+                torch.save((gaussians.capture(), iteration), scene.model_path + "/chkpnt" + str(iteration) + ".pth")
+
+        with torch.no_grad():
+            if network_gui.conn == None:
+                network_gui.try_connect(dataset.render_items)
+            while network_gui.conn != None:
+                try:
+                    net_image_bytes = None
+                    custom_cam, do_training, keep_alive, scaling_modifer, render_mode = network_gui.receive()
+                    if custom_cam != None:
+                        render_pkg = render(custom_cam, gaussians, pipe, background, scaling_modifer)
+                        net_image = render_net_image(render_pkg, dataset.render_items, render_mode, custom_cam)
+                        net_image_bytes = memoryview((torch.clamp(net_image, min=0, max=1.0) * 255).byte().permute(1, 2,
+                                                                                                                   0).contiguous().cpu().numpy())
+                    metrics_dict = {
+                        "#": gaussians.get_opacity.shape[0],
+                        "loss": ema_loss_for_log
+                        # Add more metrics as needed
+                    }
+                    # Send the data
+                    network_gui.send(net_image_bytes, dataset.source_path, metrics_dict)
+                    if do_training and ((iteration < int(opt.iterations)) or not keep_alive):
+                        break
+                except Exception as e:
+                    # raise e
+                    network_gui.conn = None
+
+
+def prepare_output_and_logger(args):
+    if not args.model_path:
+        if os.getenv('OAR_JOB_ID'):
+            unique_str = os.getenv('OAR_JOB_ID')
+        else:
+            unique_str = str(uuid.uuid4())
+        args.model_path = os.path.join("./output/", unique_str[0:10])
+
+    datasetname = args.source_path.split("/")[-2]
+    dataname = args.source_path.split("/")[-1]
+    # args.model_path = os.path.join("./output/1SH4Color", datasetname,dataname)
+
+    # Set up output folder
+    print("Output folder: {}".format(args.model_path))
+    os.makedirs(args.model_path, exist_ok=True)
+    with open(os.path.join(args.model_path, "cfg_args"), 'w') as cfg_log_f:
+        cfg_log_f.write(str(Namespace(**vars(args))))
+
+    # Create Tensorboard writer
+    tb_writer = None
+    if TENSORBOARD_FOUND:
+        tb_writer = SummaryWriter(args.model_path)
+    else:
+        print("Tensorboard not available: not logging progress")
+    return tb_writer
+
+
+@torch.no_grad()
+def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_iterations, scene: Scene, renderFunc,
+                    renderArgs, gaussians_c):
+    if tb_writer:
+        tb_writer.add_scalar('train_loss_patches/reg_loss', Ll1.item(), iteration)
+        tb_writer.add_scalar('train_loss_patches/total_loss', loss.item(), iteration)
+        tb_writer.add_scalar('iter_time', elapsed, iteration)
+        tb_writer.add_scalar('total_points', scene.gaussians.get_xyz.shape[0], iteration)
+
+
+if __name__ == "__main__":
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(2)
+    # Set up command line argument parser
+    parser = ArgumentParser(description="Training script parameters")
+    lp = ModelParams(parser)
+    op = OptimizationParams(parser)
+    pp = PipelineParams(parser)
+    parser.add_argument('--ip', type=str, default="127.0.0.1")
+    parser.add_argument('--port', type=int, default=6008)
+    parser.add_argument('--detect_anomaly', action='store_true', default=False)
+    parser.add_argument("--test_iterations", nargs="+", type=int, default=[7_000, 30_000])
+    parser.add_argument("--save_iterations", nargs="+", type=int, default=[7_000, 30_000])
+    parser.add_argument("--quiet", action="store_true")
+    parser.add_argument("--checkpoint_iterations", nargs="+", type=int, default=[])
+    parser.add_argument("--start_checkpoint", type=str, default=None)
+    parser.add_argument("--max_gs", type=int, default=9999_999_999)
+
+    args = parser.parse_args(sys.argv[1:])
+    args.save_iterations.append(args.iterations)
+
+    print("Optimizing " + args.model_path)
+
+    # Initialize system state (RNG)
+    safe_state(args.quiet)
+
+    # Start GUI server, configure and run training
+    network_gui.init(args.ip, args.port)
+    torch.autograd.set_detect_anomaly(args.detect_anomaly)
+    training(lp.extract(args), op.extract(args), pp.extract(args), args.test_iterations, args.save_iterations,
+             args.checkpoint_iterations, args.start_checkpoint)
+
+    # All done
+    print("\nTraining complete.")
